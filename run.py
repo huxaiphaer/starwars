65e4c96e49d131e5a2b0a7bfa7e2b4f9c16f99d3
huxaiphaer <huxaiphaeridris@gmail.com>
2020-01-27 20:09:13 +0300

    add tests

--- run.py
+++ run.py
@@ -118,7 +118,8 @@ def get_star_ships():
 
     known_star_ships = []
     unknown_star_ships = []
-    #call task
+
+    # call celery task
     paginate_requested_data.delay("https://swapi.co/api/starships/", redis_db)
     res = redis_db.hgetall('mydata')
 
